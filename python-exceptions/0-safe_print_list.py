519e1b211cdda8a461934e81fbbfad9267a0245a
ordes <10027@holbertonstudents.com>
2025-01-23 22:22:28 +0000

    added file

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
 
-def raise_exception_msg(message=""):
-    raise NameError(message)
+def safe_print_list(my_list=[], x=0):
+    nr_elements = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            nr_elements += 1
+        except IndexError:
+            break
+    print()
+    return nr_elements
