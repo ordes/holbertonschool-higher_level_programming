700d657041b31948e8c62ffcbea818166027ebcb
ordes <10027@holbertonstudents.com>
2025-03-06 10:28:08 +0000

    added file

---
+++ SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,9 @@
+-- Write a script that lists all Comedy shows in the database hbtn_0d_tvshows
+
+SELECT tv_shows.title AS title FROM tv_genres
+	LEFT JOIN tv_show_genres
+	ON tv_show_genres.genre_id = tv_genres.id
+		LEFT JOIN tv_shows
+		ON tv_shows.id = tv_show_genres.show_id
+		WHERE tv_genres.name = 'Comedy'
+		ORDER BY title;
---
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,5 @@
+-- script to join tables
+SELECT cities.id, cities.name, states.name
+FROM cities
+INNER JOIN states ON cities.state_id = states.id
+ORDER BY cities.id ASC;
